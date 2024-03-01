a523721bb68f14db4579b832a8df062d44e5ab63
Maamua06 <115535704+Maamua06@users.noreply.github.com>
2024-03-01 15:17:54 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Windows Server Guide</h1>
+      <video width='1000' controls>
+        <source src='https://placehold.co/1920x1080.mp4' type='video/mp4' />
+      </video>
+ 
     </div>
   );
 }
